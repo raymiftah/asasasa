9d7ad03119e0174c8748483bd99c5962fb148796
raymiftah <140677875+raymiftah@users.noreply.github.com>
2023-11-30 10:57:31 +0700

    Add files via upload

---
+++ daily.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const tasklist = document.getElementById ("task-list")
+   const newtaskinput = document.getElementById ("new-task")
+   const addtaskbutton = document.getElementById ("task-button")
+
+    addtaskbutton.addEventListener("button", function(){
+        const tasktext = newtaskinput.ariaValueMax.trim
+
+    }
