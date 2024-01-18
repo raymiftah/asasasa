913159d2e8ae0446763e922d5dfc4071b783a40c
raymiftah <140677875+raymiftah@users.noreply.github.com>
2024-01-18 11:09:57 +0700

    Add files via upload

--- ASEAN.py
+++ ASEAN.py
@@ -1,9 +1,10 @@
 ASEAN = ["INDONESIA", "MALAYSIA", "SINGAPORE", "LAOS", "FILIPINA","brunnei"]
 print(ASEAN)
 print(len(ASEAN))
-ASEAN.append("indonesia")
+ASEAN_list = list(ASEAN)
+ASEAN_list.append("indonesia")
 print(ASEAN)
 print(ASEAN[1])
-ASEAN[1] = "steak wagyu A5"
+ASEAN[1] = "brunnei darussalam"
 del ASEAN[2]
 print(ASEAN)
